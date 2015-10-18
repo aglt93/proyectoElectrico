0fdc8d27864a46aae2c0d5a35f0e372bcc1a6a44
alejandro <aglt93@gmail.com>
2015-10-18 01:18:47 -0600

    trabajando en key expansion dia 1

--- proyectoLeon/codigo/keyExpansion.v
+++ proyectoLeon/codigo/keyExpansion.v
@@ -1,4 +1,4 @@
-
+`timescale 1ns/10ps
 
 module keyBytesToWords 
 #(
@@ -10,20 +10,70 @@ module keyBytesToWords
 )
 
 (
+	input clk,
+	input rst,
 	input [w-1:0] pW,
 	input [w-1:0] qW,
-	input [8*b-1:0] key
+	input [8*b-1:0] key,
+	output [w-1:0] out0,out1,out2,out3
 );
 
-	reg [w-1:0] S [t-1:0];
-	reg [w-1:0] L [c-1:0];
-	reg i = 0;
 
-	always @(L[0]) begin 
-		repeat (b) begin
-			
-			L[i] = (L[i]<<<8) + key[i];
+	wire [w-1:0] S [t-1:0];
+	wire [w-1:0] L [c-1:0];
+	wire [7:0] keyInBytes [b-1:0];
 
-		end	  	
-	end 
+	generate
+
+		genvar i,j;
+		/*
+		for (j = 0; j < b; j = j+1) begin
+			assign keyInBytes[j]=key[8*j+7:8*j];
+		end
+		*/
+		for (i = b-1; i >= 0 ; i = i - 1) begin
+			oper1 #(w) hola (clk,rst,L[i/u],key[8*i+7:8*i],L[i/u]);
+		end
+
+	endgenerate
+
+	assign out0 = L[0];
+	assign out1 = L[1];
+	assign out2 = L[2];
+	assign out3 = L[3];
+
+endmodule
+
+module oper1 
+#(
+	parameter w = 32
+)
+
+(
+	input clk,
+	input rst,
+	input [w-1:0] wordIn,
+	input [7:0] constant,
+	output reg [w-1:0] wordOut
+);
+
+	wire [w-9:0] zeros;
+	assign zeros = 0;
+
+	reg [7:0] temporal;
+
+	always @(posedge clk or posedge rst) begin
+
+		if(rst) begin
+			wordOut = 0;
+		end
+
+		else begin
+			temporal = #1 constant;
+			wordOut = #1 {wordIn[7:0],wordIn[w-1:8]} + {zeros,temporal};	
+
+		end
+
+	end
+
 endmodule
---
+++ proyectoLeon/codigo/testbench.v
@@ -0,0 +1,52 @@
+`include "keyExpansion.v"
+`timescale 1ns/10ps
+
+`define w 32
+`define b 16
+`define t 26
+`define c 4
+
+// PROBADOR DEL PIPELINE IF_ID LISTOS.
+module probador (clk,rst,pW,qW,key);
+  
+  
+	// Salidas
+  output reg [`w-1:0] pW;
+  output reg [`w-1:0] qW;
+  output reg [8*`b-1:0] key;
+  output reg rst,clk;
+ 
+	// Entradas.
+	
+
+	initial begin
+	
+    	$dumpfile("prueba.vcd");
+		$dumpvars;
+		
+		clk = 0;
+    	pW = 10;
+    	qW = 5;
+    	key = 128'hFFFEEEE58684FFF05FFE493853000434;
+    	rst = 0;
+    	#2 rst = 1;
+    	#2 rst = 0;
+		#170 $finish;
+		
+	end
+
+	always #5 clk = ~clk;
+	
+endmodule
+
+
+module tester;
+
+  wire [`w-1:0] pW,Qw;
+  wire [8*`b-1:0] key;
+  wire [`w-1:0] out0,out1,out2,out3;
+
+  probador test(clk,rst,pW,Qw,key);
+  keyBytesToWords pegado (clk,rst,pW,Qw,key,out0,out1,out2,out3);
+
+endmodule
