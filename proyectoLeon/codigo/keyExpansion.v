a2e0e9712bfac01d89b40a45ce7619a5753aed8d
alejandro <aglt93@gmail.com>
2015-10-23 18:03:46 -0600

    paso 1 keyExpansion listo con testbench

--- proyectoLeon/codigo/keyExpansion.v
+++ proyectoLeon/codigo/keyExpansion.v
@@ -2,32 +2,37 @@
 
 module keyBytesToWords 
 #(
-	parameter b = 16, 
-	parameter t = 26, 
+	parameter b = 16,
+	parameter b_length = 4,
 	parameter w = 32,
 	parameter u = 4,
-	parameter c = 4
+	parameter c = 4,
+	parameter c_length = 2
 )
 
 (
-	input clk,
+	input clk1,
+	input clk2,
 	input rst,
 	input [7:0] key_sub_i,
 	input [w-1:0] L_sub_i,
-	output key_address,
-	output [] L_address,
-	output [w-1:0] L_sub_i_prima
+	output [b_length-1:0] key_address,
+	output [c_length-1:0] L_address,
+	output reg [w-1:0] L_sub_i_prima
 );
 
-	reg [w-1:0] temp;	
-	reg [4:0] count;
+	reg [b_length:0] count;
 
 	wire done;
 
 
 	assign done = (!count) ? 1:0;
+	assign key_address = count;
+	assign L_address = count/u;
 
-	always @(posedge clk or posedge rst) begin
+	reg [w-1:0] temp;
+
+	always @(posedge clk2 or posedge rst) begin
 
 		if(rst) begin
 			count = b;
@@ -35,9 +40,16 @@ module keyBytesToWords
 
 		else begin 
 			if (done==0) begin
-				L_sub_i_prima =  {L_sub_i[7:0],L_sub_i[w-1:0]} + key_sub_i;
-				count = count - 1;
+				temp = {L_sub_i[7:0],L_sub_i[w-1:0]};	
+				//$display("L = %h   ,  temp = %h" , L_sub_i,temp);
+				L_sub_i_prima = temp + key_sub_i;
 			end
+		end
+	end
+
+	always @(posedge clk1) begin
+		if (done==0 && rst==0) begin
+			count = count - 1;
 		end
 	end
 
--- proyectoLeon/codigo/testbench.v
+++ proyectoLeon/codigo/testbench.v
@@ -1,54 +1,131 @@
 `include "keyExpansion.v"
 `timescale 1ns/10ps
 
-`define w 32
-`define b 16
-`define t 26
-`define c 4
+`define w 32 // Cantidad de bits por palabra.
+`define u 4 // Cantidad de bytes por palabra.
+`define b 16 // Cantidad de bytes de la llave.
+`define b_length 4 // Bits para direccionar a todos los bytes b de la llave.
+`define t 26 // Cantidad de rondas.
+`define c 4 // Tamaño del vector L. Corresponde a b/u.
+`define c_length 2 // Cantidad de bits para direccionar al vector L.
 
-// PROBADOR DEL PIPELINE IF_ID LISTOS.
-module probador (clk,rst,key,L_sub_i);
-  
-  
-	// Salidas
-  output reg [8*`b-1:0] key;
-  output reg rst,clk;
- 
-	// Entradas.
-	
+module probador 
+(
+	output reg rst,
+	output reg clk1,
+	output reg clk2,
+	output reg [8*`b-1:0] key,
+	output reg [`w-1:0] L_sub_i,
+	output reg [7:0] key_sub_i,
+	input [`b_length-1:0] key_address,
+	input [`c_length-1:0] L_address,
+	input [`w-1:0] L_sub_i_prima
+);
+
+	integer i;
+
+  	reg [`w-1:0] L [`c:0];
+  	reg [7:0] keyInBytes [`b-1:0];
+
+  	`include "driver.v"
 
 	initial begin
-	
+
     	$dumpfile("prueba.vcd");
 		$dumpvars;
 		
-		clk = 0;
-    	pW = 10;
-    	qW = 5;
-    	key = 128'hFFFEEEE58684FFF05FFE493853000434;
+		key = 128'hFFFEEEE58684FFF05FFE493853000434;
+		clk1 = 0;
+
+		drv_init();
+
+		repeat(20) begin
+			fork 
+				drv_write();
+				drv_read();
+			join
+		end
+
+		/*
+		for (i=0;i<`c;i=i+1) begin
+			L[i]=0;
+		end
+
+		key = 128'hFFFEEEE58684FFF05FFE493853000434;
+
+		for (i=0;i<`b;i=i+1) begin
+			keyInBytes[i]=5+i;
+		end
+		clk1 = 0;
+
     	rst = 0;
-    	#2 rst = 1;
-    	#2 rst = 0;
-		#170 $finish;
+    	#3 rst = 1;
+    	#1 rst = 0;
+    	*/
+
+
+		#2 $finish;
 		
 	end
 
-	always #5 clk = ~clk;
-	
-	always @(posedge clk) begin
-		L_sub_i = L[]
+	always #5 clk1 = ~clk1;	
+
+	always @(*) begin
+  		clk2 = #0.5 clk1;
+  	end
+
+  	/*
+	always @(posedge clk2) begin
+		L_sub_i = L[L_address];
+		key_sub_i = keyInBytes[key_address];
 	end
 
+	always @(negedge clk2) begin
+		L[L_address] = L_sub_i_prima;	
+	end
+	*/
 
 endmodule
 
 
 module tester;
 
-  wire [8*`b-1:0] key_sub_i;
+  wire [7:0] key_sub_i;
   wire [`w-1:0] L_sub_i,L_sub_i_prima;
-
-  probador test(clk,rst,key_sub_i,L_sub_i,L_sub_i_prima);
-  keyBytesToWords pegado (clk,rst,key_sub_i,L_sub_i,L_sub_i_prima);
+  wire [`b_length-1:0] key_address;
+  wire [`c_length-1:0] L_address;
+ 
+  probador test
+  (
+  	.clk1(clk1),
+  	.clk2(clk2),
+  	.rst(rst),
+  	.key_sub_i(key_sub_i),
+  	.L_sub_i(L_sub_i),
+  	.L_sub_i_prima(L_sub_i_prima),
+  	.key_address(key_address),
+  	.L_address(L_address)
+  );
+  
+  keyBytesToWords  
+  #(
+  	.b(`b),
+  	.b_length(`b_length),
+  	.w(`w),
+  	.u(`u),
+  	.c(`c),
+  	.c_length(`c_length)
+  )
+  pegado 
+  (
+  	.clk1(clk1),
+  	.clk2(clk2),
+  	.rst(rst),
+  	.key_sub_i(key_sub_i),
+  	.L_sub_i(L_sub_i),
+  	.L_sub_i_prima(L_sub_i_prima),
+  	.key_address(key_address),
+  	.L_address(L_address)
+   );
 
 endmodule
